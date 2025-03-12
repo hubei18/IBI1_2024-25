7a8831c4579bfe8161ee345b211e135d3ca1d390
hubei18 <rui.24@intl.zju.edu.cn>
2025-03-12 14:38:30 +0800

    update bmi_calculator

--- Practical4/bmi_calculator.py
+++ Practical4/bmi_calculator.py
@@ -3,8 +3,8 @@
 # compare BMI with 18.5 and 30
 # give result 
 
-mass=int(input("mass"))
-height=int(input("height"))
+mass=float(input("mass"))
+height=float(input("height"))
 bmi=mass/height**2
 if bmi < 18.5:
     print("thin")
