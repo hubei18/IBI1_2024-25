6e1c437fa3c3da120e3c19caeb4b58567096fc55
hubei18 <rui.24@intl.zju.edu.cn>
2025-05-14 15:47:29 +0800

    add practical13

---
+++ Practical13/alignment.py
@@ -0,0 +1,26 @@
+from Bio.Align import substitution_matrices
+blosum62 = substitution_matrices.load("BLOSUM62")
+
+def read(filename):
+    sequence=''
+    with open(filename,'r') as file:
+        for line in file:
+            if line[0]!='>':
+                sequence+=line.strip()
+    return sequence
+
+def identity(seq1,seq2):
+    distance=sum(c1==c2 for c1,c2 in zip(seq1,seq2))
+    return distance/len(seq1)*100
+
+def score(seq1,seq2):
+    return sum(blosum62[c1][c2] for c1,c2 in zip(seq1,seq2))
+
+human=read('P04179.fasta.txt')
+mouse=read('P09671.fasta.txt')
+random=read('random.fasta')
+
+print(f'For human and mouse, the alignment score is {score(human,mouse)}, the percentage of identical amino acids is {identity(human,mouse)}')
+print(f'For human and random, the alignment score is {score(human,random)}, the percentage of identical amino acids is {identity(human,random)}')
+print(f'For random and mouse, the alignment score is {score(random,mouse)}, the percentage of identical amino acids is {identity(random,mouse)}')
+
